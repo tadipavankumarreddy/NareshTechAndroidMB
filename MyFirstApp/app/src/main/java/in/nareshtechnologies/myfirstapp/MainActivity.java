af5bd22c31c30b8e1c9cdd08f383f9eb1dfa39fd
Pavan Kumar Reddy Tadi <tadipavankumarreddy@gmail.com>
2022-11-24 08:17:57 +0530

    Assignment

--- MyFirstApp/app/src/main/java/in/nareshtechnologies/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/in/nareshtechnologies/myfirstapp/MainActivity.java
@@ -3,12 +3,45 @@ package in.nareshtechnologies.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Create two objects for Button class
+    Button plusBtn, minusBtn;
+
+    //Create another object for TextView class
+    TextView result;
+
+    int count = 0;
+    // We trigger the activity execution from onCreate(...)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        plusBtn = findViewById(R.id.plus_btn);
+        minusBtn = findViewById(R.id.minus_btn);
+        result = findViewById(R.id.result);
+
+        plusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               // from here I can handle the clicks on the button
+                count++;
+                result.setText(String.valueOf(count));
+            }
+        });
+
+        minusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // from here I can handle the clicks on the button
+                count--;
+                result.setText(String.valueOf(count));
+            }
+        });
     }
 }
